7b2db1e77f0a3e8891e2e83ce370df24dcfd40d2
Krysta <krystaperalto@gmail.com>
2017-11-17 11:24:43 -0500

    Updating with getFilesatUrl

--- GUIProject/QtGUI/Backend.cpp
+++ GUIProject/QtGUI/Backend.cpp
@@ -1,5 +1,6 @@
 #include "Backend.h"
 
+string data;
 //TODO updateCategoryExtension method
 //CourseCategory Backend::updateCategoryExtension(CourseCategory categoryObject);
 
@@ -34,8 +35,131 @@ void Backend::daemonUpdateTask(int updateInterval) {
    }
 }
 
+
+
+size_t writeCallback(char* buf,size_t size, size_t nmemb, void* up){
+    for (int c =0; c<size*nmemb; c++){
+        data.push_back(buf[c]);
+        //cout<< buf[c];
+    }
+    return size*nmemb;
+}
+
+bool webpageError(CURL *curl, string line){
+    if (line.find("401 Authorization Required") != -1){
+        //TODO Try Website with Authorization
+        return true;
+    }
+    else if (line.find("301 Moved Permanently") != -1 ){
+        data="";
+
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void curlAtUrl(CURL* curl, string url){
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
+
+    curl_easy_perform(curl);
+}
+
 //TODO getFilesAtUrl, can use CourseScraper methods as example but use better structure.
-//vector<string> Backend::getFilesAtUrl(CourseCategory categoryObject);
+
+vector<string> Backend::getFilesAtUrl(CourseCategory categoryObject){
+
+    data = ""; //do I need a copy constructor? overload the assignment operator?
+    string urlPath = categoryObject.getUrlPath(); //a set url from the database
+    char url [100];
+    strcpy(url, urlPath.c_str());
+    //vector<string> fileExtensions = categoryObject.getExtensionPreferences();
+    vector<string> fileExtensions;
+    fileExtensions.push_back(".pdf");
+    fileExtensions.push_back(".cpp");
+    fileExtensions.push_back(".zip");
+    fileExtensions.push_back(".txt");
+    fileExtensions.push_back(".pptx");
+    fileExtensions.push_back(".h");
+
+    CURL* curl;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
+    //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    curl_easy_perform(curl);
+
+    stringstream ss_init(data);
+    string line;
+    while(getline(ss_init, line)){
+        if (webpageError(curl,line)){
+            break;
+        }
+    }
+
+    stringstream ss(data);
+    vector<string> allFiles, moreFiles;
+
+
+    if (find(urlsVisited.begin(), urlsVisited.end(), url) == urlsVisited.end()){ //puts the root url
+        urlsVisited.push_back(url);
+    }
+
+    while (getline(ss, line)){
+        if (line.find(".html\"") != -1){
+            unsigned first = line.find('"');
+            unsigned last = line.find(".html\"");
+            string newUrl(url);
+            newUrl.erase(newUrl.find_last_of("/"));
+            newUrl += "/" + line.substr(first+1, last-first+4);
+            if(urlValid(newUrl)){
+                urlsVisited.push_back(newUrl);
+                cout << url << endl;
+                cout << newUrl << endl;
+                //TODO call getFilesAtUrl with new URL
+                CourseCategory c = CourseCategory("intmUrl", newUrl, fileExtensions);
+                moreFiles = getFilesAtUrl(c);
+                allFiles.insert(allFiles.end(),moreFiles.begin(), moreFiles.end());
+            }
+        }
+        for (int i = 0; i < (int)fileExtensions.size(); i++){
+            if (line.find(fileExtensions[i]+'"') != -1){
+                unsigned first = line.find('"');
+                unsigned last = line.find(fileExtensions[i]+'"');
+                string file = line.substr(first+1, last-first+(fileExtensions[i].length())-1);
+                allFiles.push_back(file);
+                cout << file << endl;
+            }
+        }
+
+    }
+
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+
+    for (auto i : allFiles){
+        cout << i << endl;
+    }
+
+    return allFiles;
+}
+
+bool Backend::urlValid(string newUrl){
+    if (find(urlsVisited.begin(), urlsVisited.end(), newUrl) == urlsVisited.end()){
+       string fullUrl = newUrl + '/';
+       if (find(urlsVisited.begin(), urlsVisited.end(), fullUrl) == urlsVisited.end()){
+           if(fullUrl.find("/#") == -1){
+               return true;
+           }
+       }
+    }
+    return false;
+}
 
 //TODO downloadFilesInUrl, can use CourseScraper methods as example but use better structure.
 //void Backend::downloadFilesInUrl(CourseCategory categoryObject);
--- GUIProject/QtGUI/Backend.h
+++ GUIProject/QtGUI/Backend.h
@@ -3,6 +3,9 @@
 #include "Course.h"
 #include <string.h>
 #include <vector>
+#include <curl/curl.h>
+#include <boost/filesystem.hpp>
+#include <boost/regex.h>
 
 class Backend;
 
@@ -13,10 +16,16 @@ public:
     static CourseCategory updateCategoryExtension(CourseCategory categoryObject);
     static void downloadFilesForCourse(Course courseObject);
     static void daemonUpdateTask(int updateInterval);
-
-private:
     vector<string> getFilesAtUrl(CourseCategory categoryObject);
+    vector<string> urlsVisited;
+private:
+    bool urlValid(string newUrl);
     void downloadFilesInUrl(CourseCategory categoryObject);
-
 };
+
+size_t writeCallback(char* buf,size_t size, size_t nmemb, void* up);
+bool webpageError(CURL* curl, string line);
+void curlAtUrl(CURL* curl, string url);
+extern string data;
+
 
